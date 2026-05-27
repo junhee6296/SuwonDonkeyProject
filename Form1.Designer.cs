d9c5fde85a993de2eb8755c0519f7a527e537f8e
지혁 박 <jhboy0714@gmail.com>
2026-05-27 11:32:12 +0900

    ui구성 및 1차 기능 구현 간단한 ui 구성 현재 구현한 기능에 대한 ui만 간단하게 구성한 상태임. 폴더 선택 버튼 기능 구현 : 폴더를 선택하면 자동으로 버튼 옆 textbox에 폴더 경로가 표시됨. listbox에 이미지 파일들의 명칭들이 업데이트됨. 기본적으로 0번 인덱스가 선택되어 해당 인덱스의 이미지를 중앙에 표시하고 그 옆에 인덱스 번호, 해당 인덱스의 throttle과 angle 값을 표시함. 인덱스 선택 기능 구현 : listbox의 항목을 눌러 선택 인덱스를 바꿀 수 있고, 트랙바로도 인덱스를 바꿀 수 있음. >, < 버튼을 눌러 인덱스를 1씩 바꿀 수 있음. 삭제 버튼 기능 구현 : 삭제 버튼을 누르면 현재 인덱스의 이미지, catalog파일의 데이터를 전부 삭제함. 되돌리기 버튼 기능 구현 : 직전에 진행한 삭제 작업 1개를 되돌림. 값 수정 기능, 저장 버튼 기능 구현 : 값 수정 그룹에서 throttle과 angle에 원하는 값을 입력하고 저장 버튼을 누르면 입력한 값이 catalog파일에 적용됨.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,161 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            btnMember1 = new Button();
-            btnMember2 = new Button();
-            btnMember3 = new Button();
-            btnMember4 = new Button();
-            txtName = new TextBox();
-            txtSchool = new TextBox();
-            txtClass = new TextBox();
+            btnSelectFolder = new Button();
+            lstIndexList = new ListBox();
+            txtSelectedFolder = new TextBox();
+            picCurrentIndexImage = new PictureBox();
+            trbChangeIndex = new TrackBar();
+            btnBefore = new Button();
+            btnAfter = new Button();
+            btnDelete = new Button();
+            btnSave = new Button();
+            grpEditValue = new GroupBox();
+            txtChangeAngle = new TextBox();
+            txtChangeThrottle = new TextBox();
+            lblChangeAngle = new Label();
+            lblChangeThrottle = new Label();
+            lblCurrentIndex = new Label();
+            lblCurrentThrottle = new Label();
+            lblCurrentAngle = new Label();
+            btnUndo = new Button();
+            ((System.ComponentModel.ISupportInitialize)picCurrentIndexImage).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trbChangeIndex).BeginInit();
+            grpEditValue.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // btnSelectFolder
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("맑은 고딕", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            label1.Location = new Point(24, 15);
-            label1.Name = "label1";
-            label1.Size = new Size(263, 65);
-            label1.TabIndex = 0;
-            label1.Text = "Team App";
+            btnSelectFolder.Location = new Point(12, 22);
+            btnSelectFolder.Name = "btnSelectFolder";
+            btnSelectFolder.Size = new Size(94, 29);
+            btnSelectFolder.TabIndex = 0;
+            btnSelectFolder.Text = "폴더선택";
+            btnSelectFolder.UseVisualStyleBackColor = true;
+            btnSelectFolder.Click += btnSelectFolder_Click;
             // 
-            // label2
+            // lstIndexList
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("맑은 고딕", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            label2.ForeColor = Color.Blue;
-            label2.Location = new Point(33, 116);
-            label2.Name = "label2";
-            label2.Size = new Size(96, 50);
-            label2.TabIndex = 1;
-            label2.Text = "이름";
+            lstIndexList.FormattingEnabled = true;
+            lstIndexList.Location = new Point(12, 67);
+            lstIndexList.Name = "lstIndexList";
+            lstIndexList.Size = new Size(175, 504);
+            lstIndexList.TabIndex = 1;
             // 
-            // label3
+            // txtSelectedFolder
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("맑은 고딕", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            label3.ForeColor = Color.Blue;
-            label3.Location = new Point(33, 197);
-            label3.Name = "label3";
-            label3.Size = new Size(96, 50);
-            label3.TabIndex = 2;
-            label3.Text = "학교";
-            label3.Click += label3_Click;
+            txtSelectedFolder.Location = new Point(112, 22);
+            txtSelectedFolder.Name = "txtSelectedFolder";
+            txtSelectedFolder.Size = new Size(401, 27);
+            txtSelectedFolder.TabIndex = 2;
+            txtSelectedFolder.Text = "mycar 폴더를 선택해주세요";
             // 
-            // label4
+            // picCurrentIndexImage
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("맑은 고딕", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            label4.ForeColor = Color.Blue;
-            label4.Location = new Point(33, 275);
-            label4.Name = "label4";
-            label4.Size = new Size(96, 50);
-            label4.TabIndex = 3;
-            label4.Text = "학년";
+            picCurrentIndexImage.Location = new Point(203, 67);
+            picCurrentIndexImage.Name = "picCurrentIndexImage";
+            picCurrentIndexImage.Size = new Size(310, 293);
+            picCurrentIndexImage.TabIndex = 3;
+            picCurrentIndexImage.TabStop = false;
             // 
-            // btnMember1
+            // trbChangeIndex
             // 
-            btnMember1.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            btnMember1.Location = new Point(466, 99);
-            btnMember1.Name = "btnMember1";
-            btnMember1.Size = new Size(118, 47);
-            btnMember1.TabIndex = 4;
-            btnMember1.Text = "멤버1";
-            btnMember1.UseVisualStyleBackColor = true;
+            trbChangeIndex.Location = new Point(203, 371);
+            trbChangeIndex.Name = "trbChangeIndex";
+            trbChangeIndex.Size = new Size(310, 56);
+            trbChangeIndex.TabIndex = 4;
+            trbChangeIndex.Scroll += trackBar1_Scroll;
             // 
-            // btnMember2
+            // btnBefore
             // 
-            btnMember2.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            btnMember2.Location = new Point(466, 163);
-            btnMember2.Name = "btnMember2";
-            btnMember2.Size = new Size(118, 47);
-            btnMember2.TabIndex = 5;
-            btnMember2.Text = "멤버2";
-            btnMember2.UseVisualStyleBackColor = true;
+            btnBefore.Location = new Point(203, 412);
+            btnBefore.Name = "btnBefore";
+            btnBefore.Size = new Size(36, 29);
+            btnBefore.TabIndex = 5;
+            btnBefore.Text = "<";
+            btnBefore.UseVisualStyleBackColor = true;
+            btnBefore.Click += btnBefore_Click;
             // 
-            // btnMember3
+            // btnAfter
             // 
-            btnMember3.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            btnMember3.Location = new Point(466, 229);
-            btnMember3.Name = "btnMember3";
-            btnMember3.Size = new Size(118, 47);
-            btnMember3.TabIndex = 6;
-            btnMember3.Text = "멤버3";
-            btnMember3.UseVisualStyleBackColor = true;
+            btnAfter.Location = new Point(245, 412);
+            btnAfter.Name = "btnAfter";
+            btnAfter.Size = new Size(36, 29);
+            btnAfter.TabIndex = 6;
+            btnAfter.Text = ">";
+            btnAfter.UseVisualStyleBackColor = true;
+            btnAfter.Click += btnAfter_Click;
             // 
-            // btnMember4
+            // btnDelete
             // 
-            btnMember4.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            btnMember4.Location = new Point(466, 295);
-            btnMember4.Name = "btnMember4";
-            btnMember4.Size = new Size(118, 47);
-            btnMember4.TabIndex = 7;
-            btnMember4.Text = "멤버4";
-            btnMember4.UseVisualStyleBackColor = true;
+            btnDelete.BackColor = Color.Red;
+            btnDelete.ForeColor = SystemColors.ButtonFace;
+            btnDelete.Location = new Point(380, 412);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(66, 29);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "삭제";
+            btnDelete.UseVisualStyleBackColor = false;
             // 
-            // txtName
+            // btnUndo
             // 
-            txtName.Font = new Font("맑은 고딕", 24F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            txtName.Location = new Point(142, 118);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(280, 50);
-            txtName.TabIndex = 8;
+            btnUndo.BackColor = Color.White;
+            btnUndo.ForeColor = Color.Black;
+            btnUndo.Location = new Point(447, 412);
+            btnUndo.Name = "btnUndo";
+            btnUndo.Size = new Size(78, 29);
+            btnUndo.TabIndex = 13;
+            btnUndo.Text = "되돌리기";
+            btnUndo.UseVisualStyleBackColor = false;
             // 
-            // txtSchool
+            // btnSave
             // 
-            txtSchool.Font = new Font("맑은 고딕", 24F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            txtSchool.Location = new Point(142, 197);
-            txtSchool.Name = "txtSchool";
-            txtSchool.Size = new Size(280, 50);
-            txtSchool.TabIndex = 9;
+            btnSave.BackColor = Color.Lime;
+            btnSave.Location = new Point(447, 412);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(66, 29);
+            btnSave.TabIndex = 8;
+            btnSave.Text = "저장";
+            btnSave.UseVisualStyleBackColor = false;
             // 
-            // txtClass
+            // grpEditValue
             // 
-            txtClass.Font = new Font("맑은 고딕", 24F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            txtClass.Location = new Point(142, 275);
-            txtClass.Name = "txtClass";
-            txtClass.Size = new Size(280, 50);
-            txtClass.TabIndex = 10;
+            grpEditValue.Controls.Add(txtChangeAngle);
+            grpEditValue.Controls.Add(txtChangeThrottle);
+            grpEditValue.Controls.Add(lblChangeAngle);
+            grpEditValue.Controls.Add(lblChangeThrottle);
+            grpEditValue.Location = new Point(203, 445);
+            grpEditValue.Name = "grpEditValue";
+            grpEditValue.Size = new Size(310, 126);
+            grpEditValue.TabIndex = 9;
+            grpEditValue.TabStop = false;
+            grpEditValue.Text = "값 수정";
+            // 
+            // txtChangeAngle
+            // 
+            txtChangeAngle.Location = new Point(85, 80);
+            txtChangeAngle.Name = "txtChangeAngle";
+            txtChangeAngle.Size = new Size(196, 27);
+            txtChangeAngle.TabIndex = 3;
+            // 
+            // txtChangeThrottle
+            // 
+            txtChangeThrottle.Location = new Point(85, 43);
+            txtChangeThrottle.Name = "txtChangeThrottle";
+            txtChangeThrottle.Size = new Size(196, 27);
+            txtChangeThrottle.TabIndex = 2;
+            // 
+            // lblChangeAngle
+            // 
+            lblChangeAngle.AutoSize = true;
+            lblChangeAngle.Location = new Point(20, 80);
+            lblChangeAngle.Name = "lblChangeAngle";
+            lblChangeAngle.Size = new Size(47, 20);
+            lblChangeAngle.TabIndex = 1;
+            lblChangeAngle.Text = "angle";
+            // 
+            // lblChangeThrottle
+            // 
+            lblChangeThrottle.AutoSize = true;
+            lblChangeThrottle.Location = new Point(20, 43);
+            lblChangeThrottle.Name = "lblChangeThrottle";
+            lblChangeThrottle.Size = new Size(59, 20);
+            lblChangeThrottle.TabIndex = 0;
+            lblChangeThrottle.Text = "throttle";
+            // 
+            // lblCurrentIndex
+            // 
+            lblCurrentIndex.AutoSize = true;
+            lblCurrentIndex.Location = new Point(519, 67);
+            lblCurrentIndex.Name = "lblCurrentIndex";
+            lblCurrentIndex.Size = new Size(50, 20);
+            lblCurrentIndex.TabIndex = 10;
+            lblCurrentIndex.Text = "label1";
+            // 
+            // lblCurrentThrottle
+            // 
+            lblCurrentThrottle.AutoSize = true;
+            lblCurrentThrottle.Location = new Point(519, 87);
+            lblCurrentThrottle.Name = "lblCurrentThrottle";
+            lblCurrentThrottle.Size = new Size(50, 20);
+            lblCurrentThrottle.TabIndex = 11;
+            lblCurrentThrottle.Text = "label1";
+            // 
+            // lblCurrentAngle
+            // 
+            lblCurrentAngle.AutoSize = true;
+            lblCurrentAngle.Location = new Point(519, 107);
+            lblCurrentAngle.Name = "lblCurrentAngle";
+            lblCurrentAngle.Size = new Size(50, 20);
+            lblCurrentAngle.TabIndex = 12;
+            lblCurrentAngle.Text = "label1";
+            // 
+            // btnUndo
+            // 
+            btnUndo.BackColor = Color.White;
+            btnUndo.ForeColor = Color.Black;
+            btnUndo.Location = new Point(296, 412);
+            btnUndo.Name = "btnUndo";
+            btnUndo.Size = new Size(78, 29);
+            btnUndo.TabIndex = 13;
+            btnUndo.Text = "되돌리기";
+            btnUndo.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(611, 383);
-            Controls.Add(txtClass);
-            Controls.Add(txtSchool);
-            Controls.Add(txtName);
-            Controls.Add(btnMember4);
-            Controls.Add(btnMember3);
-            Controls.Add(btnMember2);
-            Controls.Add(btnMember1);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            ClientSize = new Size(837, 592);
+            Controls.Add(btnUndo);
+            Controls.Add(lblCurrentAngle);
+            Controls.Add(lblCurrentThrottle);
+            Controls.Add(lblCurrentIndex);
+            Controls.Add(grpEditValue);
+            Controls.Add(btnSave);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAfter);
+            Controls.Add(btnBefore);
+            Controls.Add(trbChangeIndex);
+            Controls.Add(picCurrentIndexImage);
+            Controls.Add(txtSelectedFolder);
+            Controls.Add(lstIndexList);
+            Controls.Add(btnSelectFolder);
+            Margin = new Padding(4);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)picCurrentIndexImage).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trbChangeIndex).EndInit();
+            grpEditValue.ResumeLayout(false);
+            grpEditValue.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Button btnMember1;
-        private Button btnMember2;
-        private Button btnMember3;
-        private Button btnMember4;
-        private TextBox txtName;
-        private TextBox txtSchool;
-        private TextBox txtClass;
+        private Button btnSelectFolder;
+        private ListBox lstIndexList;
+        private TextBox txtSelectedFolder;
+        private PictureBox picCurrentIndexImage;
+        private TrackBar trbChangeIndex;
+        private Button btnBefore;
+        private Button btnAfter;
+        private Button btnDelete;
+        private Button btnSave;
+        private GroupBox grpEditValue;
+        private TextBox txtChangeAngle;
+        private TextBox txtChangeThrottle;
+        private Label lblChangeAngle;
+        private Label lblChangeThrottle;
+        private Label lblCurrentIndex;
+        private Label lblCurrentThrottle;
+        private Label lblCurrentAngle;
+        private Button btnUndo;
     }
 }
